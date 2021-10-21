bfeb075ae8b754dc1c45fd26171a7ceee209c9f9
magdasokolovic <magda.sokolovic@gmail.com>
2021-10-21 13:23:54 +0200

    finished webpack configuration.

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,14 +1,11 @@
-const path = require('path');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const {merge} = require('webpack-merge')
+const path = require('path')
+
+const { merge } = require('webpack-merge')
 const config = require('./webpack.config')
 
 module.exports = merge(config, {
-    node: "production",
-    output: {
-        path: path.join(__dirname, "public")
-    },
-    plugins: [
-        new CleanWebpackPlugin()
-    ]
+  node: 'production',
+  output: {
+    path: path.join(__dirname, 'public')
+  }
 })
